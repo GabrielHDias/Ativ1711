1e727660a802315ed730753a96dbbaad0a9f9cdb
GabrielHDias <eng.ghdias@gmail.com>
2025-11-24 10:07:35 -0300

    começando dnv

---
+++ fag/Principal.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import fag.objetos.Funcionarios;
+import fag.objetos.Construcao
+
+public class Principal
+
+public static void main(String[] args) {
+
+}
---
+++ fag/objetos/Funcionarios.java
@@ -0,0 +1,8 @@
+/*todo? func/nome, cargo e salario
+objeto construcao/nome da obra, endereço e data de inicio*/
+import fag.Principal
+import fag.objetos.Construcao
+
+public class Funcionarios
+
+public static void
